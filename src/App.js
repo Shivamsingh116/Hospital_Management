fe8ba2e127d0f6e4bbbd305c8da7974b357c5772
Shivamsingh116 <solankishivam116@gmail.com>
2024-09-27 20:43:18 +0530

    Update in repo

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+function App() {
+  return (
+    <h1 className='hello'>
+      Hello,Start ur Project
+    </h1>)
+}
+
+export default App;
---
+++ src/NavCompont/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Nav.css';
+function Nav() {
+    return (
+        <div className="navContainer">
+            <div className="logoContainer">
+                {/* <img src="https://www.pngkit.com/png/full/131-1318043_hospitals-transparent-hospital-icon-white.png" alt="#" /> */}
+                <h1>Hospi</h1>
+                <img src="https://png.pngtree.com/png-vector/20230209/ourmid/pngtree-hospital-logo-png-image_6591586.png" alt="#" />
+                <h1>al</h1>
+            </div>
+
+        </div>)
+}
+export default Nav
